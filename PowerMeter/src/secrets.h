69af24f7d60b84dde2c40c3af5c8aea74f2e6cf5
mihugo <mike.git@m3h.com>
2020-07-12 10:31:12 -0700

    various bug fixes and remote control

--- PowerMeter/src/secrets.h
+++ PowerMeter/src/secrets.h
@@ -1,4 +1,4 @@
-#ifndef _SECRETS_H
+  #ifndef _SECRETS_H
 #define _SECRETS_H
 
 #define NUMAPS 4
@@ -12,11 +12,21 @@ static char const *APs[NUMAPS][2] = {
 // For connecting to the MQTT Broker
 #define MQTT_Server    "192.168.1.17"
 #define MQTT_Port      1883
+#define MQTT_ClientID  "me"
+// #define MQTT_TelemetryTopic_Root "iot/device/""
+#define MQTT_TelemetryTopic_Root "v1/devices/"
+//#define MQTT_UserID    "ESP8266_PowerMeter"
+//#define MQTT_ClientID  "ESP8266_PowerMeter"
 #define MQTT_ClientID  "ESP8266_PowerMeter"
 #define MQTT_UserID    "ESP8266_PowerMeter"
 #define MQTT_Password  "password1"
+#define MQTT_Password  ""
 
 // For the UDP Log Server
+#define UDPLOG_Server  "192.168.1.251"     // My local PC, but should be a server"
+#define UDPLOG_Port    514
+
+#define HOSTNAME "PumpPowerMeter"
 #define UDPLOG_Server  "192.168.1.68"     // My local PC, but should be a server"
 #define UDPLOG_Port    5014
 
